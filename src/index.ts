ba2dfec0418d7c632b2f7ab6d93f8d2486b9c508
Chintan Koradiya <chintan.koradiya@tatvasoft.com>
2026-01-08 16:29:49 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("TypeScript Docker CI/CD v1 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
